14eecad159c753ddab084569d2ffc7c32feed59d
cristianbgp <cristian.granda.pastor@gmail.com>
2019-08-27 13:17:30 -0500

    Map data from tambo api

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,14 +3,22 @@ class StoresController < ApplicationController
   require 'open-uri'
 
   def index
-    # 1 pollo
-    # 2 cajero
-    # 3 24 horas
-    # 4 fritanga
+    #Tambo+ stores services
+    # 1 - pollo, 2 - cajero, 3 - 24 horas, 4 - fritanga
     url = "https://tambomas.pe/public/api/stores"
     data = JSON.parse(open(url).read)
-    p data["stores"].size
-    render json: data
+    result = data["stores"].map do |store|
+      {
+        id: store["_id"],
+        name: store["name"],
+        longitude: store["longitude"], 
+        latitude: store["latitude"],
+        address: store["address"],
+        allday: store["services"].include?("3"),
+        atm: store["services"].include?("2")
+      }
+    end
+    render json: result
   end
 
 end
